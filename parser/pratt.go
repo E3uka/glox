aa18c71066385eb804edf3ee739370b28fed0a42
Ebuka Agbanyim <ebuka7@outlook.com>
2022-12-10 19:00:06 +0000

    add credits for pratt algorithm and restructure + annotation for clear understanding

--- parser/pratt.go
+++ parser/pratt.go
@@ -21,7 +21,6 @@ func NewPrattParser(path *string, tokens *[]token.Token) *pratt {
 		current: 0,
 		expr:    nil,
 	}
-
 	return pratt
 }
 
@@ -34,6 +33,10 @@ func (p *pratt) parse_statement_expression(
 ) []ast.StatementExpr {
 	var statement []ast.StatementExpr
 	for !p.isAtEnd() {
+		// wrapped in anonymous function so block is called as unit ~ enables
+		// panic recovery to occur mutiple times instead of short-circuiting on
+		// first capture; recovery advances to start of next viable statement
+		// token which continues the parse operation from that point
 		func() {
 			defer func() {
 				if r := recover(); r != nil {
@@ -41,6 +44,8 @@ func (p *pratt) parse_statement_expression(
 					sync_next_stmt(p)
 				}
 			}()
+
+			// may panic
 			stmt := nd_parse_many_statement(p, p.peek())
 			statement = append(statement, stmt)
 		}()
@@ -48,6 +53,8 @@ func (p *pratt) parse_statement_expression(
 	return statement
 }
 
+// Top Down Operator Precedence - Vaughan R. Pratt, 1973
+// https://dl.acm.org/doi/10.1145/512927.512931
 func (p *pratt) parse_expression(cur_prec precedence) ast.Expr {
 	var left ast.Expr
 	cur_tok := p.peek()
@@ -58,7 +65,6 @@ func (p *pratt) parse_expression(cur_prec precedence) ast.Expr {
 		cur_tok = p.peek()
 		left = left_deno[cur_tok.Type](p, cur_tok.Type, left)
 	}
-
 	return left
 }
 
@@ -97,61 +103,63 @@ var (
 	left_deno map[token.TOKEN_TYPE]func(*pratt, token.TOKEN_TYPE, ast.Expr) ast.Expr
 )
 
-// init maps and populate with the appropriate parser dispatch functions
+// initialize maps and populate with associated parser dispatch function with
+// respect to token type, precedence (binding power) and parse location (denotation)
 func init() {
 	prec_map = make(map[token.TOKEN_TYPE]precedence)
 	null_deno = make(map[token.TOKEN_TYPE]func(*pratt, token.Token) ast.Expr)
 	left_deno = make(map[token.TOKEN_TYPE]func(*pratt, token.TOKEN_TYPE, ast.Expr) ast.Expr)
 
-	prec_map[token.LET] = LOWEST
 	prec_map[token.ASSIGN] = LOWEST
+	prec_map[token.LET] = LOWEST
 	prec_map[token.EQL] = EQUALITY
 	prec_map[token.NEQ] = EQUALITY
-	prec_map[token.GTR] = LESSGREATER
 	prec_map[token.GEQ] = LESSGREATER
-	prec_map[token.LSS] = LESSGREATER
+	prec_map[token.GTR] = LESSGREATER
 	prec_map[token.LEQ] = LESSGREATER
-	prec_map[token.ADD] = ADD
-	prec_map[token.INCR] = UNARY
-	prec_map[token.INCRBY] = ADD
-	prec_map[token.SUB] = SUB
-	prec_map[token.DECR] = UNARY
+	prec_map[token.LSS] = LESSGREATER
 	prec_map[token.DECRYBY] = SUB
+	prec_map[token.SUB] = SUB
+	prec_map[token.ADD] = ADD
+	prec_map[token.INCRBY] = ADD
 	prec_map[token.MUL] = MUL
 	prec_map[token.QUO] = QUO
+	prec_map[token.DECR] = UNARY
+	prec_map[token.INCR] = UNARY
 	prec_map[token.IDENT] = PRIMARY
-
-	null_deno[token.FLOAT] = nd_parse_literal
-	null_deno[token.STRING] = nd_parse_literal
-	null_deno[token.NIL] = nd_parse_literal
-	null_deno[token.TRUE] = nd_parse_literal
-	null_deno[token.FALSE] = nd_parse_literal
-
-	// TODO add logic for statements next so nil dereference is not thrown
-	// null_deno[token.IDENT] = nd_parse_ident
-
-	null_deno[token.SUB] = nd_parse_unary
-	null_deno[token.NOT] = nd_parse_unary
 
 	null_deno[token.LPAREN] = nd_parse_grouping
 
+	// TODO add logic for statements next so nil dereference is not thrown
+	// should be a runtime error if the identity does not resolve
+	// null_deno[token.IDENT] = nd_parse_ident
+
+	null_deno[token.FALSE] = nd_parse_literal
+	null_deno[token.FLOAT] = nd_parse_literal
+	null_deno[token.NIL] = nd_parse_literal
+	null_deno[token.STRING] = nd_parse_literal
+	null_deno[token.TRUE] = nd_parse_literal
+
 	null_deno[token.LET] = nd_parse_statement
 
-	left_deno[token.INCR] = ld_parse_unary
-	left_deno[token.DECR] = ld_parse_unary
+	null_deno[token.NOT] = nd_parse_unary
+	null_deno[token.SUB] = nd_parse_unary
 
-	left_deno[token.EQL] = ld_parse_binary
-	left_deno[token.NEQ] = ld_parse_binary
-	left_deno[token.GTR] = ld_parse_binary
-	left_deno[token.GEQ] = ld_parse_binary
-	left_deno[token.LSS] = ld_parse_binary
-	left_deno[token.LEQ] = ld_parse_binary
+	left_deno[token.DECR] = ld_parse_unary
+	left_deno[token.INCR] = ld_parse_unary
+
 	left_deno[token.ADD] = ld_parse_binary
-	left_deno[token.INCRBY] = ld_parse_binary
-	left_deno[token.SUB] = ld_parse_binary
 	left_deno[token.DECRYBY] = ld_parse_binary
+	left_deno[token.EQL] = ld_parse_binary
+	left_deno[token.GEQ] = ld_parse_binary
+	left_deno[token.GTR] = ld_parse_binary
+	left_deno[token.INCRBY] = ld_parse_binary
+	left_deno[token.LEQ] = ld_parse_binary
+	left_deno[token.LSS] = ld_parse_binary
 	left_deno[token.MUL] = ld_parse_binary
+	left_deno[token.NEQ] = ld_parse_binary
 	left_deno[token.QUO] = ld_parse_binary
+	left_deno[token.SUB] = ld_parse_binary
 }
 
 func nd_parse_literal(parser *pratt, tok token.Token) ast.Expr {
@@ -231,8 +239,8 @@ func ld_parse_binary(
 func sync_next_stmt(parser *pratt) {
 	parser.advance()
 	for !parser.isAtEnd() {
+		// found end statement boundary
 		if parser.peek().Type == token.SEMICOLON {
-			// already at next statement boundary
 			return
 		}
 		switch parser.peek().Type {
@@ -240,7 +248,7 @@ func sync_next_stmt(parser *pratt) {
 			token.RETURN, token.WHILE:
 			return
 		}
-		// go past current token until boundary is found
+		// advance until boundary is found
 		parser.advance()
 	}
 }
